0fa0aea4985020f03d8ac00b9d92542bbc01f64c
AndrewGavril <gavrilov.andrew1999@yandex.ru>
2019-02-08 19:59:04 +0300

    Adding new sender

--- packetSender.c
+++ packetSender.c
@@ -1,37 +1,46 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <string.h>
 #include <netinet/in.h>
-#include <inttypes.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
 
-int main()
+#define PORT 12345
+#define SERVADDR "fe80::3c26:ad45:e02d:c12a"
+
+int main(void)
 {
-	char *message = (char*)malloc(6*sizeof(char));
-	int dest_port = 2455;
-	struct sockaddr_in6 dest_addr;
-	int sockfd;
+  int sock;
+  socklen_t clilen;
+  struct sockaddr_in6 server_addr, client_addr;
+  char buffer[1024];
+  char addrbuf[INET6_ADDRSTRLEN];
 
-	sockfd = socket(PF_INET6, SOCK_DGRAM, 0);
+  sock = socket(PF_INET6, SOCK_DGRAM, 0);
 
-	dest_addr.sin6_family = AF_INET6;
-	dest_addr.sin6_port = htons(dest_port);
-	inet_pton("fe80::24ca:5cff:fe6b:5da6",  &dest_addr.sin6_addr);
+  if (sock < 0) {
+    perror("creating socket");
+    exit(1);
+  }
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin6_family = AF_INET6;
+  inet_pton(AF_INET6, SERVADDR, &server_addr.sin6_addr);
 
-	int i = 0;
-	while (1){
-		sleep(5);
-		i++;
+  server_addr.sin6_port = htons(PORT);
+  char msg[50];
+  printf("Write massage:\n");
+  fgets(msg,50,stdin);
+  msg[strlen(msg)-1]='\0';
 
-		if (i%3 == 1)
-			message = "send  ";
-		else if (i%3 == 2)
-			message = "delete";
-		else message = "make";
-		printf("Sending '%s' \n", message);
-		sendto (sockfd, message, strlen(message)+1, 0 , (struct sockaddr*)&dest_addr, sizeof(struct sockaddr_in6));
-	}
-	return 0;
+  if (sendto(sock, msg, sizeof(msg), 0,
+             (struct sockaddr *)&server_addr,
+	     sizeof(server_addr)) < 0) {
+      perror("sendto failed");
+      exit(4);
+  }
+  close(sock);
+
+  return 0;
 }
